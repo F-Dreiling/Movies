5c878d2c65f09a1c10621f679c1e0273640df38c
FX Drooklyn <dr.f380@hotmail.com>
2023-11-08 15:44:51 +0100

    Testing

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
+    //baseURL:'https://localhost:8080'
     baseURL:'https://communal-garfish-intensely.ngrok-free.app/',
     headers: {"ngrok-skip-browser-warning": "true"}
 });
