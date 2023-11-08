fe7206f3460a400dfd084dd92ba3bcb00df096fc
FX Drooklyn <dr.f380@hotmail.com>
2023-11-08 18:18:49 +0100

    setup

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    //baseURL:'https://localhost:8080'
-    baseURL:'https://communal-garfish-intensely.ngrok-free.app/',
-    headers: {"ngrok-skip-browser-warning": "true"}
+    baseURL:'http://localhost:8080'
+    //baseURL:'http://dreiling.dev:8080'
+    //baseURL:'https://communal-garfish-intensely.ngrok-free.app/',
+    //headers: {"ngrok-skip-browser-warning": "true"}
 });
